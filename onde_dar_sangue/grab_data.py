ef5d75bffe29118378051e7382f2bed8d0dcaf45
Filipe Barroso <filipe.barroso@gmail.com>
2020-02-16 15:02:51 +0000

    Remove debug loggers

--- onde_dar_sangue/grab_data.py
+++ onde_dar_sangue/grab_data.py
@@ -33,9 +33,6 @@ class GrabData:
             need
         '''
         script_with_data = all_scripts[22]
-        logger.debug(script_with_data)
         raw_data = script_with_data.get_text()[2638:22314]
-        logger.debug(raw_data)
         data = json.loads(raw_data)
-        logger.debug(data)
         return data
